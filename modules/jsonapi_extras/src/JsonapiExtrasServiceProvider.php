25a4b4b2547ebfb6b9080737b097257695403873
Biaoge <349255833@qq.com>
2019-07-28 23:00:56 +0800

    update drupal to 8.7.5

--- modules/jsonapi_extras/src/JsonapiExtrasServiceProvider.php
+++ modules/jsonapi_extras/src/JsonapiExtrasServiceProvider.php
@@ -41,7 +41,20 @@ class JsonapiExtrasServiceProvider extends ServiceProviderBase {
     // \Drupal\jsonapi\Serializer\Serializer::__construct().
     $container_namespaces = $container->getParameter('container.namespaces');
     $container_modules = $container->getParameter('container.modules');
-    $container_namespaces['Drupal\jsonapi\Normalizer\ImpostorFrom\jsonapi_extras'][] = dirname($container_modules['jsonapi_extras']['pathname']) . '/src-impostor-normalizers';
+    $jsonapi_impostor_path = dirname($container_modules['jsonapi_extras']['pathname']) . '/src-impostor-normalizers';
+    $container_namespaces['Drupal\jsonapi\Normalizer\ImpostorFrom\jsonapi_extras'][] = $jsonapi_impostor_path;
+    // Manually include the impostor definitions to avoid class not found error
+    // during compilation, which gets triggered though cache-clear.
+    $container->getDefinition('serializer.normalizer.field_item.jsonapi_extras')
+      ->setFile($jsonapi_impostor_path . '/FieldItemNormalizerImpostor.php');
+    $container->getDefinition('serializer.normalizer.resource_identifier.jsonapi_extras')
+      ->setFile($jsonapi_impostor_path . '/ResourceIdentifierNormalizerImpostor.php');
+    $container->getDefinition('serializer.normalizer.resource_object.jsonapi_extras')
+      ->setFile($jsonapi_impostor_path . '/ResourceObjectNormalizerImpostor.php');
+    $container->getDefinition('serializer.normalizer.content_entity.jsonapi_extras')
+      ->setFile($jsonapi_impostor_path . '/ContentEntityDenormalizerImpostor.php');
+    $container->getDefinition('serializer.normalizer.config_entity.jsonapi_extras')
+      ->setFile($jsonapi_impostor_path . '/ConfigEntityDenormalizerImpostor.php');
     $container->setParameter('container.namespaces', $container_namespaces);
   }
 
